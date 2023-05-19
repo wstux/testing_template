5217e8d820fb808003eda18010abd546c8225dce
Chistyakov Alexander <wstux1@gmail.com>
2023-05-19 21:23:44 +0300

    + libs: #13 Fixed compilation errors;

--- libs/testing/testing_interface.h
+++ libs/testing/testing_interface.h
@@ -74,6 +74,127 @@ struct Types
                              T12, T13, T14, T15> type;
 };
 
+template <>
+struct Types<details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t>
+{
+    typedef details::type_0 type;
+};
+template <typename T1>
+struct Types<T1, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t>
+{
+    typedef details::type_1<T1> type;
+};
+template <typename T1, typename T2>
+struct Types<T1, T2, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t>
+{
+    typedef details::type_2<T1, T2> type;
+};
+template <typename T1, typename T2, typename T3>
+struct Types<T1, T2, T3, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t>
+{
+    typedef details::type_3<T1, T2, T3> type;
+};
+template <typename T1, typename T2, typename T3, typename T4>
+struct Types<T1, T2, T3, T4, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t>
+{
+    typedef details::type_4<T1, T2, T3, T4> type;
+};
+template <typename T1, typename T2, typename T3, typename T4, typename T5>
+struct Types<T1, T2, T3, T4, T5, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t>
+{
+    typedef details::type_5<T1, T2, T3, T4, T5> type;
+};
+template <typename T1, typename T2, typename T3, typename T4, typename T5,
+          typename T6>
+struct Types<T1, T2, T3, T4, T5, T6, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t>
+{
+    typedef details::type_6<T1, T2, T3, T4, T5, T6> type;
+};
+template <typename T1, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7>
+struct Types<T1, T2, T3, T4, T5, T6, T7, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t, details::none_t>
+{
+    typedef details::type_7<T1, T2, T3, T4, T5, T6, T7> type;
+};
+template <typename T1, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8>
+struct Types<T1, T2, T3, T4, T5, T6, T7, T8, details::none_t, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t>
+{
+    typedef details::type_8<T1, T2, T3, T4, T5, T6, T7, T8> type;
+};
+template <typename T1, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8, typename T9>
+struct Types<T1, T2, T3, T4, T5, T6, T7, T8, T9, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t,
+             details::none_t>
+{
+    typedef details::type_9<T1, T2, T3, T4, T5, T6, T7, T8, T9> type;
+};
+template <typename T1, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8, typename T9, typename T10>
+struct Types<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, details::none_t,
+             details::none_t, details::none_t, details::none_t, details::none_t>
+{
+    typedef details::type_10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> type;
+};
+template <typename T1, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8, typename T9, typename T10,
+          typename T11>
+struct Types<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, details::none_t,
+             details::none_t, details::none_t, details::none_t>
+{
+    typedef details::type_11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> type;
+};
+template <typename T1, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8, typename T9, typename T10,
+          typename T11, typename T12>
+struct Types<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12,
+             details::none_t, details::none_t, details::none_t>
+{
+    typedef details::type_12<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10,
+                             T11, T12> type;
+};
+template <typename T1, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8, typename T9, typename T10,
+          typename T11, typename T12, typename T13>
+struct Types<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
+             details::none_t, details::none_t>
+{
+    typedef details::type_13<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10,
+                             T11, T12, T13> type;
+};
+template <typename T1, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8, typename T9, typename T10,
+          typename T11, typename T12, typename T13, typename T14>
+struct Types<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
+             details::none_t>
+{
+    typedef details::type_14<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10,
+                             T11, T12, T13, T14> type;
+};
+
 } // namespace testing
 
 #endif /* _TESTING_TESTING_INTERFACE_H */
