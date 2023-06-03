11eaca1093670db386e5be879cd713d483303ce6
Chistyakov Alexander <wstux1@gmail.com>
2023-06-03 10:16:38 +0300

    + libs/testing: #21 Added C++11 support;

--- libs/testing/utils.h
+++ libs/testing/utils.h
@@ -1,11 +1,17 @@
 #ifndef _TESTING_UTILS_H
 #define _TESTING_UTILS_H
 
+#ifdef __unix__
+    #include <sys/stat.h>
+    #include <dirent.h>
+    #include <errno.h>
+#endif
+
 #include <cstdio>
 #if __cplusplus >= 201703L
     #include <filesystem>
+    #include <string_view>
 #endif
-#include <string_view>
 #include <system_error>
 #include <string>
 
@@ -13,22 +19,125 @@
 
 namespace testing {
 namespace utils {
+namespace details {
+
+#if __cplusplus < 201703L
+    void create_directory(const std::string& dir, std::error_code& err)
+    {
+    #ifdef __unix__
+        struct ::stat s;
+        if (::stat(dir.c_str(), &s) == -1) {
+            ::mkdir(dir.c_str(), 0700);
+        } else {
+            err = std::make_error_code(std::errc::file_exists);
+        }
+    #else
+        #error "is_directory_exists: platform not supported"
+    #endif
+    }
+#endif
+
+inline bool is_directory_exists(const std::string& dir)
+{
+#if __cplusplus >= 201703L
+    return std::filesystem::exists(dir);
+#else
+    #ifdef __unix__
+        ::DIR* dir = ::opendir(dir.c_str());
+        if (dir) {
+            ::closedir(dir);
+            return true;
+        } else if (::errno == ENOENT) {
+            /* Directory does not exist. */
+            return false;
+        }
+        /* opendir() failed for some other reason. */
+        return false;
+    #else
+        #error "is_directory_exists: platform not supported"
+    #endif
+#endif
+}
+
+void remove_directory_recursively(const std::string& dir, std::error_code& err)
+{
+#if __cplusplus >= 201703L
+    std::filesystem::remove_all(dir, err);
+#else
+    #ifdef __unix__
+        ::DIR* dir = ::opendir(dir.c_str());
+        if (! dir) {
+            err = std::make_error_code(std::errc::no_such_file_or_directory);
+            return;
+        }
+        struct dirent* ent;
+        while (! err && (ent = ::readdir(dir))) {
+            if (! ::strcmp(ent->d_name, ".") || ! ::strcmp(ent->d_name, "..")) {
+                continue;
+            }
+            const std::string ent_path = dir + "/" + ent->d_name;
+            struct ::stat s;
+            if (::stat(ent_path.c_str(), &s) != 0) {
+                err = std::make_error_code(std::errc::io_error);
+                break;
+            }
+            if (S_ISDIR(s.st_mode)) {
+                remove_directory_recursively(ent_path, err);
+            } else {
+                if (::unlink(ent_path.c_str()) != 0) {
+                    err = std::make_error_code(std::errc::operation_not_permitted);
+                }
+            }
+        }
+        ::closedir(dir);
+        if (! err) {
+            if (::rmdir(dir.c_str()) != 0) {
+                err = std::make_error_code(std::errc::operation_not_permitted);
+            }
+        }
+    #else
+        #error "remove_directory_recursively: platform not supported"
+    #endif
+#endif
+}
+
+#if __cplusplus < 201703L
+    std::string temp_directory_path(std::error_code& err)
+    {
+    #ifdef __unix__
+        const char* p_folder = ::getenv("TMPDIR");
+        if (p_folder == NULL) {
+            err = std::make_error_code(std::errc::no_such_file_or_directory);
+        }
+        return p_folder;
+    #else
+        #error "remove_directory_recursively: platform not supported"
+    #endif
+    }
+#endif
+
+} // namespace details
 
 /*
  * Funstions.
  */
 
-#if __cplusplus >= 201703L
-inline std::filesystem::path create_tmp_dir(const std::string& prefix);
+inline std::string create_tmp_dir(const std::string& prefix);
 inline bool remove_dir(const std::filesystem::path& dir);
-#endif
 inline std::string unique_path(std::string prefix = std::string());
 
-#if __cplusplus >= 201703L
-inline std::filesystem::path create_tmp_dir(const std::string& prefix)
+inline std::string create_tmp_dir(const std::string& prefix)
 {
+#if __cplusplus >= 201703L
+    namespace fs = std::filesystem;
+    using path_t = fs::path;
+#else
+    namespace fs = ::testing::utils::details;
+    using path_t = std::string;
+#endif
+
     std::error_code err;
-    std::filesystem::path tmp_dir = std::filesystem::temp_directory_path(err);
+    path_t tmp_dir = fs::temp_directory_path(err);
     if (err) {
         return {};
     }
@@ -37,24 +146,47 @@ inline std::filesystem::path create_tmp_dir(const std::string& prefix)
     if (tmp_directory_name.empty()) {
         return {};
     }
+#if __cplusplus >= 201703L
     tmp_dir /= tmp_directory_name;
-    std::filesystem::create_directory(tmp_dir, err);
+#else
+    tmp_dir += "/" + tmp_directory_name;
+#endif
+    fs::create_directory(tmp_dir, err);
     if (err) {
         return {};
     }
-    return tmp_dir;
+    return tmp_dir.string();
 }
 
-inline bool remove_dir(const std::filesystem::path& dir)
+inline bool remove_dir(const std::string& dir)
 {
-    if (! std::filesystem::exists(dir)) {
+    if (! details::is_directory_exists(dir)) {
         return false;
     }
 
     std::error_code err;
-    std::filesystem::remove_all(dir, err);
+    details::remove_directory_recursively(dir, err);
     return (! err);
 }
+
+#ifdef __unix__
+    inline int mem_usage()
+    {
+        static const std::string statm_path = "/proc/self/statm";
+        unsigned long dummu;
+        unsigned long resident;
+
+        FILE *f = ::fopen(statm_path.c_str(), "r");
+        if (!f) {
+            return -1;
+        }
+
+        if (::fscanf(f,"%lu %lu", &dummu, &resident) != 2) {
+            resident = -1;
+        }
+        ::fclose(f);
+        return resident;
+    }
 #endif
 
 inline std::string unique_path(std::string prefix)
@@ -80,7 +212,6 @@ inline std::string unique_path(std::string prefix)
  * Classes.
  */
 
-#if __cplusplus >= 201703L
 class base_tester : public ::testing::Test
 {
 public:
@@ -97,11 +228,11 @@ public:
 
     virtual void TearDown() override { remove_dir(m_work_dir); }
 
-    const std::filesystem::path& work_dir() const { return m_work_dir; }
+    const std::string& work_dir() const { return m_work_dir; }
 
 private:
     const std::string m_test_name;
-    std::filesystem::path m_work_dir;
+    std::string m_work_dir;
 };
 
 class base_env : public ::testing::Environment
@@ -120,13 +251,12 @@ public:
 
     virtual void TearDown() override { remove_dir(m_work_dir); }
 
-    const std::filesystem::path& work_dir() const { return m_work_dir; }
+    const std::string& work_dir() const { return m_work_dir; }
 
 private:
     const std::string m_test_name;
-    std::filesystem::path m_work_dir;
+    std::string m_work_dir;
 };
-#endif
 
 } // namespace utils
 } // namespace testing
